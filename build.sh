43197722151edc559ade36584e4712aa2f1f7d6e
sriramjsr001 <130354902+sriramjsr001@users.noreply.github.com>
2023-10-22 05:06:19 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ docker build -t "$IMAGE_TAG" .
 # Check if the build was successful
 if [ $? -eq 0 ]; then
   echo "Docker image '$IMAGE_TAG' successfully built."
+  docker push sriramjsr1997/dev
 else
   echo "Failed to build Docker image."
 fi
-docker push sriramjsr1997/dev
+
 
